3f34abb3ce841b3418392f072dd6805e15cb457a
Ash Blue <ash@blueashes.com>
2016-05-10 15:19:01 -0600

    Added SCSS compilation with watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
+/**
+ * Gulp compildation for development tools
+ * @author Ash Blue
+ * @TODO Exclude sourcemaps during production mode
+ * @TODO If production mode Ugflify compiled files
+ */
+
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var webserver = require('gulp-webserver');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps'); // Sourcemaps must be manually generated due to a bug with typescript gulp compilation
+var sass = require('gulp-sass');
 
+// Node packages
 var del = require('del');
 var path = require('path');
 
+// Custom packages
 var tsProject = ts.createProject('src/tsconfig.json');
 var bootstrap = require('./src/build-config');
 
+// Constants
 const SRC_ROOT = 'src';
+const SRC_STYLES = SRC_ROOT + '/styles';
 const FOLDER_DIST = 'dist';
 const FOLDER_TMP = 'tmp';
 
@@ -20,6 +32,14 @@ const FOLDER_TMP = 'tmp';
   // Do we need JSCS?
   // Do we need JSHint?
 // });
+
+gulp.task('compile-css', function () {
+  return gulp.src(SRC_STYLES + '/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(FOLDER_TMP));
+});
 
 gulp.task('copy-html', function () {
   return gulp
@@ -55,12 +75,13 @@ gulp.task('serve', function () {
     }));
 });
 
-// @TODO If production mode Ugflify compiled files
 gulp.task('copy-dependencies', function () {
   var bundle = bootstrap.dependencies.core.concat(bootstrap.dependencies.other);
 
   return gulp.src(bundle)
+    .pipe(sourcemaps.init())
     .pipe(concat('dependencies.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(FOLDER_TMP + '/'));
 });
 
@@ -68,7 +89,7 @@ gulp.task('watch', function () {
   gulp.watch('src/**/*.ts', ['compile-ts']);
   gulp.watch('src/index.html', ['copy-html']);
   gulp.watch('src/build-config', ['copy-dependencies']);
-  // gulp.watch('src/**/*.css', ['css']);
+  gulp.watch(SRC_STYLES + '/**/*.scss', ['compile-css']);
 });
 
 function clearFolder (folder) {
@@ -85,4 +106,4 @@ gulp.task('mode-prod', function () {
 
 gulp.task('default', ['dev']);
 gulp.task('dev', ['build', 'watch', 'serve']);
-gulp.task('build', ['clear-tmp', 'compile-ts', 'copy-html', 'copy-dependencies']);
+gulp.task('build', ['clear-tmp', 'compile-ts', 'copy-html', 'compile-css', 'copy-dependencies']);
