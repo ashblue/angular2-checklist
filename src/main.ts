66293b3f24c3e5674a937db5bf04771e69068764
Ash Blue <ash@blueashes.com>
2016-05-17 16:13:15 -0600

    Enabled Angular 2 prod mode in production only

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,11 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {Router} from './app/router';
 import versionManager from './app/utilities/version-manager';
+import {enableProdMode} from '@angular/core';
+
+if (ENV.environment === 'prod') {
+  enableProdMode();
+}
 
 versionManager.checkVersion();
 bootstrap(Router);
