39a364722f5768ca37e1c11de3fca3ea6920e146
Ash Blue <ash@blueashes.com>
2016-05-17 11:21:32 -0600

    Added global variables for typescript

---
+++ src/globals.ts
@@ -0,0 +1,9 @@
+/**
+ * Include all global variable declarations typescript needs to know about here
+ */
+
+declare var ENV: any;
+declare var $: any;
+declare var _: any;
+declare var uuid: any;
+declare var moment: any;
